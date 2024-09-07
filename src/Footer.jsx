da3c62476bfb195edc1ff08d45b441a0068d8ae2
Yuval Shar <yuval.shar.10@gmail.com>
2024-09-07 20:08:43 +0300

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; Your website name</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href = "#">Home</a></li>
+                    <li><a href = "#">About</a></li>
+                    <li><a href = "#">Services</a></li>
+                    <li><a href = "#">Contant</a></li>
+                </ul>
+            </nav> 
+            <hr/>
+        </header>
+    );
+}
+
+export default Header
