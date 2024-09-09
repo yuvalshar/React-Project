b56a06e67fee294a38160c0de09d408e91196874
Yuval Shar <yuval.shar.10@gmail.com>
2024-09-09 15:47:54 +0300

    Deleting Second-React-Project

--- src/Second-React-Project/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card() {
-    return(
-        <div className = "card">
-            <img></img>
-            <h2>Yuval Shar</h2>
-            <p>I'm developing React project</p>
-        </div>
-    )
-}
