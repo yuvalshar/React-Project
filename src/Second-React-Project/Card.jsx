e7eb1ead2568b722ea140c7c2a92df36e89ec7de
Yuval Shar <yuval.shar.10@gmail.com>
2024-09-09 15:24:39 +0300

    adding new Card component and change project port using JS

---
+++ src/Second-React-Project/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return(
+        <div className = "card">
+            <img></img>
+            <h2>Yuval Shar</h2>
+            <p>I'm developing React project</p>
+        </div>
+    )
+}
