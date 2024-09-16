cd49bd8afe267df0204101ddf5ddbf9fe2eee9e1
Yuval Shar <yuval.shar.10@gmail.com>
2024-09-16 20:10:48 +0300

    Nothing to commit

--- src/Render-Lists/src/List.jsx
+++ src/Render-Lists/src/List.jsx
@@ -2,7 +2,7 @@ function List() {
     
     const fruit = [{id: 1, name: "apple", clories: 95},
                    {id: 2, name: "orange", clories: 47},
-                   {id: 3, name: "coconut", clories: 157},
+                   {id: 3, name: "coconut", clories: 159},
                    {id: 4, name: "lemon", clories: 29},
                    {id: 5, name: "watermelon", clories: 30}];
 
