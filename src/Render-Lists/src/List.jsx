1b7d918baa0b7561e64ba6f03487d3055d608903
Yuval Shar <yuval.shar.10@gmail.com>
2024-09-21 11:03:18 +0300

    Adding map methods and organizing the list

--- src/Render-Lists/src/List.jsx
+++ src/Render-Lists/src/List.jsx
@@ -1,12 +1,22 @@
 function List() {
     
-    const fruit = [{id: 1, name: "apple", clories: 95},
-                   {id: 2, name: "orange", clories: 47},
-                   {id: 3, name: "coconut", clories: 159},
-                   {id: 4, name: "lemon", clories: 29},
-                   {id: 5, name: "watermelon", clories: 30}];
+const fruits = [{id: 1, name: "apple", calories: 95},
+                   {id: 2, name: "orange", calories: 47},
+                   {id: 3, name: "coconut", calories: 159},
+                   {id: 4, name: "lemon", calories: 29},
+                   {id: 5, name: "watermelon", calories: 30}];
 
-    const listItems = fruit.map(fruits => <li key = {fruits.id}>{fruits.name}</li>);
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); //REVERSE ALPHABRTICAL
+    fruits.sort((a, b) => a.calories - b.calories); //NUMERIC
+    // fruits.sort((a, b) => b.calories - a.calories); //REVERSE NUMERIC
+    
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    // const fruits = fruits.filter(fruit => fruit.calories >= 100);
+    
+    const listItems = fruits.map(fruit =>     <li key = {fruit.id}>
+                                                    {fruit.name}: &nbsp;
+                                                   <b>{fruit.calories}</b></li>);
     
     return(<ul>{listItems}</ul>);
     
